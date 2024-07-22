7d25ea79077f1da0db4d78c78be056f8029a75ad
JohanPedersen11 <144024830+JohanPedersen11@users.noreply.github.com>
2024-07-23 00:07:53 +0200

    keybinds can now be saved
    saving is now possible. Only obstacle left is saving multiple keys

--- Assets/_Scripts/Player/RebindingKeys.cs
+++ Assets/_Scripts/Player/RebindingKeys.cs
@@ -4,14 +4,24 @@ using UnityEngine.InputSystem;
 
 public class RebindingKeys : MonoBehaviour
 {
-    [SerializeField] private InputActionReference _interactAction;
-    [SerializeField] private PlayerController _playerController;
-    [SerializeField] private TextMeshProUGUI _bindingDisplayText;
-    [SerializeField] private GameObject _startRebinding;
-    [SerializeField] private GameObject _waitingForInput;
+    [SerializeField] private InputActionReference _interactAction; // The input key 
+    [SerializeField] private PlayerController _playerController; // Get reference to the player input
+    [SerializeField] private GameObject _startRebinding; // The button to be selected for rebinding
+    [SerializeField] private TextMeshProUGUI _bindingDisplayText; // The button text
+    [SerializeField] private GameObject _waitingForInput; // Text saying waiting for input
+
+    private const string _rebindsKey = "rebinds"; // Rebinds stored as string to json
+    private const string _defaultRebindsKey = "defaultRebinds"; // Default rebinds stored as string to json
 
     private InputActionRebindingExtensions.RebindingOperation _rebindingOperation;
-    public void StartRebinding()
+
+    private void Start()
+    {
+        SaveDefaultBindings(); // Called the first time to remember default settings. 
+        LoadRebinds();       
+    }
+
+    public void StartRebinding() // Called from button
     {
         _playerController.PlayerInput.SwitchCurrentActionMap("No Input");
 
@@ -27,9 +37,7 @@ public class RebindingKeys : MonoBehaviour
     
     private void RebindComplete()
     {
-        int bindingIndex = _interactAction.action.GetBindingIndexForControl(_interactAction.action.controls[0]);
-        _bindingDisplayText.text = InputControlPath.ToHumanReadableString(_interactAction.action.bindings[bindingIndex].effectivePath,
-            InputControlPath.HumanReadableStringOptions.OmitDevice);
+        UpdateBindingText();
 
         _rebindingOperation.Dispose();
 
@@ -37,5 +45,56 @@ public class RebindingKeys : MonoBehaviour
         _waitingForInput.SetActive(false);
 
         _playerController.PlayerInput.SwitchCurrentActionMap("Player");
+    }
+
+    public void SaveRebinds() // Called from button
+    {
+        string rebinds = _playerController.PlayerInput.actions.SaveBindingOverridesAsJson();
+        PlayerPrefs.SetString(_rebindsKey, rebinds);
+        PlayerPrefs.Save();
+        Debug.Log("saved");
+    }
+
+    private void LoadRebinds()
+    {
+        string rebinds = PlayerPrefs.GetString(_rebindsKey, string.Empty);
+
+        if (string.IsNullOrEmpty(rebinds)) // If dont have stored rebinds
+        {
+            Debug.Log("no rebinds");
+            return; // Return nothing
+        }
+
+        _playerController.PlayerInput.actions.LoadBindingOverridesFromJson(rebinds); // Get the stored rebinds from json
+        UpdateBindingText(); // Display the settings
+        Debug.Log("loaded rebinds");
+    }
+
+    public void LoadDefault() // Button. Set to default by deleteing preferences
+    {
+        string defaultRebinds = PlayerPrefs.GetString(_defaultRebindsKey, string.Empty);
+
+        _playerController.PlayerInput.actions.LoadBindingOverridesFromJson(defaultRebinds); // Load default bindings
+        UpdateBindingText(); // Display the settings
+        SaveRebinds(); // Save default settings
+        Debug.Log("loaded default rebinds");
+    }
+
+    private void SaveDefaultBindings()
+    {
+        if (!PlayerPrefs.HasKey(_defaultRebindsKey)) // Check if default bindings are already saved
+        {
+            string defaultRebinds = _playerController.PlayerInput.actions.SaveBindingOverridesAsJson();
+            PlayerPrefs.SetString(_defaultRebindsKey, defaultRebinds);
+            PlayerPrefs.Save();
+            Debug.Log("saved default rebinds");
+        }
+    }
+
+    private void UpdateBindingText()
+    {
+        int bindingIndex = _interactAction.action.GetBindingIndexForControl(_interactAction.action.controls[0]);
+        _bindingDisplayText.text = InputControlPath.ToHumanReadableString(_interactAction.action.bindings[bindingIndex].effectivePath,
+            InputControlPath.HumanReadableStringOptions.OmitDevice);
     }
 }
