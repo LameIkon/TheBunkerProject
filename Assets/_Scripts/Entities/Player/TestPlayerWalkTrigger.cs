0454cc01b11c5ac5170068f0d53994bd46bd0ef0
JohanPedersen11 <Johan.munch.2002@gmail.com>
2025-04-28 14:50:22 +0200

    walking step trigger

---
+++ Assets/_Scripts/Entities/Player/TestPlayerWalkTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerWalkTrigger : MonoBehaviour
+{
+    public void Step()
+    {
+        Debug.Log("step");
+    }
+
+    public void AnotherStep()
+    {
+        Debug.Log("another step");
+    }
+}
